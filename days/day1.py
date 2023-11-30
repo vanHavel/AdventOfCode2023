62fdad7e5ebb9130f02b4f876606292ed463416a
Lukas <dev.lukas.huwald@gmail.com>
2023-11-30 22:40:50 +0100

    initial

---
+++ days/day1.py
@@ -0,0 +1,22 @@
+from aocd import get_data, submit
+
+DAY = 1
+YEAR = 2023
+
+
+def part1(data: str) -> str:
+    pass
+
+
+def part2(data: str) -> str:
+    pass
+
+
+if __name__ == '__main__':
+    input_data = get_data(day=DAY, year=YEAR)
+    ans1 = part1(input_data)
+    print(ans1)
+    submit(answer=ans1, day=DAY, year=YEAR, part=1)
+    # ans2 = part2(input_data)
+    # print(ans2)
+    # submit(answer=ans1, day=DAY, year=YEAR, part=2)
